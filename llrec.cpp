88a1e3ff6b8088e2aa09100f219a2cdc36b5dbb6
Viviana Herrera <vh89445@usc.edu>
2025-03-08 02:28:25 +0000

    Completed correct

--- llrec.cpp
+++ llrec.cpp
@@ -6,49 +6,78 @@
 //recursive function
 void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot)
 {
-	if(head == nullptr)
+	static bool initialized = false;
+	if(!initialized)
 	{
 		smaller = nullptr;
 		larger = nullptr;
+		initialized = true;
+	}
+
+	if(head == nullptr)
+	{
+		initialized = false;
 		return;
 	}
 
 	Node* curr = head;
 	head = head->next;
 
+	llpivot(head, smaller, larger, pivot);
+
 	if(curr->val <= pivot)
 	{
-		if(smaller == nullptr)
-		{
-			smaller = curr;
-		}
-		else
-		{
-			Node* temp = smaller;
-			while(temp->next != nullptr)
-			{
-				temp = temp->next;
-			}
-			temp->next = curr;
-		}
+		curr->next = smaller;
+		smaller = curr;
 	}
 	else
 	{
-		if(larger == nullptr)
-		{
-			larger = curr;
-		}
-		else
-		{
-			Node* temp = larger;
-			while(temp->next != nullptr)
-			{
-				temp = temp->next;
-			}
-			temp->next = curr;
-		}
+		curr->next = larger;
+		larger = curr;
 	}
-	curr->next = nullptr;
+	// if(head == nullptr)
+	// {
+	// 	smaller = nullptr;
+	// 	larger = nullptr;
+	// 	return;
+	// }
+
+	// Node* curr = head;
+	// head = head->next;
+
+	// if(curr->val <= pivot)
+	// {
+	// 	if(smaller == nullptr)
+	// 	{
+	// 		smaller = curr;
+	// 	}
+	// 	else
+	// 	{
+	// 		Node* temp = smaller;
+	// 		while(temp->next != nullptr)
+	// 		{
+	// 			temp = temp->next;
+	// 		}
+	// 		temp->next = curr;
+	// 	}
+	// }
+	// else
+	// {
+	// 	if(larger == nullptr)
+	// 	{
+	// 		larger = curr;
+	// 	}
+	// 	else
+	// 	{
+	// 		Node* temp = larger;
+	// 		while(temp->next != nullptr)
+	// 		{
+	// 			temp = temp->next;
+	// 		}
+	// 		temp->next = curr;
+	// 	}
+	// }
+	// curr->next = nullptr;
 	
-	llpivot(head, smaller, larger, pivot);
+	// llpivot(head, smaller, larger, pivot);
 }
